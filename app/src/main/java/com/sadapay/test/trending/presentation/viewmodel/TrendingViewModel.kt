bbc0d8e6ebc9a73d4a17493f43c2942c555170e0
Marcus Freitas <marcus.freitas@e-therapists.de>
2023-04-16 12:58:24 +0200

    Added TrendingViewModel Test file; Added test to check for valid getTrendingRepos result;

--- app/src/main/java/com/sadapay/test/trending/presentation/viewmodel/TrendingViewModel.kt
+++ app/src/main/java/com/sadapay/test/trending/presentation/viewmodel/TrendingViewModel.kt
@@ -1,4 +1,15 @@
 package com.sadapay.test.trending.presentation.viewmodel
 
-class TrendingViewModel {
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class TrendingViewModel : ViewModel() {
+
+    val trendingReposState: MutableState<String?> = mutableStateOf(null)
+
+    fun getTrendingRepos() {
+
+    }
+
 }
---
+++ app/src/test/java/com/sadapay/test/trending/presentation/viewmodel/TrendingViewModelTest.kt
@@ -0,0 +1,29 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.sadapay.test.trending.presentation.viewmodel
+
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class TrendingViewModelTest {
+
+    private lateinit var viewModel: TrendingViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = TrendingViewModel()
+    }
+
+    @Test
+    fun `getTrendingRepos should return a valid result`() = runTest {
+        // when
+        viewModel.getTrendingRepos()
+
+        // then
+        assertNotNull(viewModel.trendingReposState.value)
+    }
+
+}
